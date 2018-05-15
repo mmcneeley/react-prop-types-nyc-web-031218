fcabe97f61245d85537deeb08074534359594ffd
mmcneeley <mmcneeley@gmail.com>
2018-05-15 12:09:01 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 // Code Goes Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Order from './components/Order'
+
+ReactDOM.render(
+  <Order />, document.getElementById('root')
+);
